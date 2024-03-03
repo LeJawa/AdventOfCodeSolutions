d09f3535545693d3c3a55320b9f806249474efe3
Luis Escobar Sawa <6599003+LeJawa@users.noreply.github.com>
2024-03-03 16:57:29 +0100

    Start day 15 2015

---
+++ src/2015/15/part2.py
@@ -0,0 +1,23 @@
+######################################
+# Solution for part 2 of day 15/2015 #
+#                                    #
+#  Started:     2024/03/03           #
+#  Finished:    ----/--/--           #
+######################################
+
+from src.common.load_file import load_file
+from src.common.function_import import import_function
+
+year, day = 2015, 15
+input: list[str] = load_file(year, day)
+
+def run() -> None:
+    result = 0
+    
+    
+    
+    return str(result)
+
+if __name__ == "__main__":
+    result = run()
+    print(f"Part 2 day {day} - {year}\nResult: {result}\n")
