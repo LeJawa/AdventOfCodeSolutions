983b760a34ab753c21bdc8d193828d66bd77519b
Luis Escobar Sawa <6599003+LeJawa@users.noreply.github.com>
2024-08-22 19:47:06 +0200

    Finish day 15 2015

--- src/2015/15/part1.py
+++ src/2015/15/part1.py
@@ -2,21 +2,82 @@
 # Solution for part 1 of day 15/2015 #
 #                                    #
 #  Started:     2024/03/03           #
-#  Finished:    ----/--/--           #
+#  Finished:    2024/08/22           #
 ######################################
 
 from src.common.load_file import load_file
 from src.common.function_import import import_function
+import re
 
 year, day = 2015, 15
 input: list[str] = load_file(year, day)
 
+
+def get_score(
+    i: int, j: int, k: int, l: int, ingredients: list[tuple[int, int, int, int]]
+) -> int:
+    c = (
+        i * ingredients[0][0]
+        + j * ingredients[1][0]
+        + k * ingredients[2][0]
+        + l * ingredients[3][0]
+    )
+    d = (
+        i * ingredients[0][1]
+        + j * ingredients[1][1]
+        + k * ingredients[2][1]
+        + l * ingredients[3][1]
+    )
+    f = (
+        i * ingredients[0][2]
+        + j * ingredients[1][2]
+        + k * ingredients[2][2]
+        + l * ingredients[3][2]
+    )
+    t = (
+        i * ingredients[0][3]
+        + j * ingredients[1][3]
+        + k * ingredients[2][3]
+        + l * ingredients[3][3]
+    )
+
+    return c * d * f * t if c > 0 and d > 0 and f > 0 and t > 0 else 0
+
+
+def brute_force(ingredients: list[tuple[int, int, int, int]]):
+    if len(ingredients) != 4:
+        return 0
+
+    max_score = -1
+
+    for i in range(100):
+        for j in range(100):
+            if i + j > 100:
+                break
+            for k in range(100):
+                if i + j + k > 100:
+                    break
+                l = 100 - (i + j + k)
+
+                score = get_score(i, j, k, l, ingredients)
+                if score > max_score:
+                    max_score = score
+
+    return max_score
+
+
 def run() -> None:
-    result = 0
-    
-    
-    
+    ingredients: list[tuple[int, int, int, int]] = []
+    for line in input:
+        c, d, f, t = re.match(
+            r".*: capacity (.+), durability (.+), flavor (.+), texture (.+),", line
+        ).groups()
+        ingredients.append((int(c), int(d), int(f), int(t)))
+
+    result = brute_force(ingredients)
+
     return str(result)
+
 
 if __name__ == "__main__":
     result = run()
--- src/2015/15/part2.py
+++ src/2015/15/part2.py
@@ -1,22 +1,64 @@
 ######################################
 # Solution for part 2 of day 15/2015 #
 #                                    #
-#  Started:     2024/03/03           #
-#  Finished:    ----/--/--           #
+#  Started:     2024/08/22           #
+#  Finished:    2024/08/22           #
 ######################################
 
+import re
 from src.common.load_file import load_file
 from src.common.function_import import import_function
 
 year, day = 2015, 15
 input: list[str] = load_file(year, day)
 
+get_score = import_function(2015, 15, 1, "get_score")
+
+
+def brute_force(ingredients: list[tuple[int, int, int, int]]):
+    if len(ingredients) != 4:
+        return 0
+
+    max_score = -1
+
+    for i in range(100):
+        for j in range(100):
+            if i + j > 100:
+                break
+            for k in range(100):
+                if i + j + k > 100:
+                    break
+                l = 100 - (i + j + k)
+
+                calories = (
+                    i * ingredients[0][4]
+                    + j * ingredients[1][4]
+                    + k * ingredients[2][4]
+                    + l * ingredients[3][4]
+                )
+                if calories != 500:
+                    break
+
+                score = get_score(i, j, k, l, ingredients)
+                if score > max_score:
+                    max_score = score
+
+    return max_score
+
+
 def run() -> None:
-    result = 0
-    
-    
-    
+    ingredients: list[tuple[int, int, int, int]] = []
+    for line in input:
+        c, d, f, t, cal = re.match(
+            r".*: capacity (.+), durability (.+), flavor (.+), texture (.+), calories (.+)",
+            line,
+        ).groups()
+        ingredients.append((int(c), int(d), int(f), int(t), int(cal)))
+
+    result = brute_force(ingredients)
+
     return str(result)
+
 
 if __name__ == "__main__":
     result = run()
