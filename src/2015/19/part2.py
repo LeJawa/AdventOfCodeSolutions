c55af3d85c5922794873f3c6343e29b3e013a346
Luis Escobar Sawa <6599003+LeJawa@users.noreply.github.com>
2024-10-18 19:10:24 +0000

    First try at day 19 2015 part 2

--- src/2015/19/part2.py
+++ src/2015/19/part2.py
@@ -14,16 +14,48 @@ parse_input: Callable[[list[str]], tuple[list[tuple[str, str]], str]]
 parse_input = import_function(2015, 19, 1, "parse_input")
 
 year, day = 2015, 19
-input: list[str] = load_test_file(year, day)
+input: list[str] = load_file(year, day)
 
+def parse_replacements(replacements: list[tuple[str, str]]) -> dict[str, str]:
+    replacement_dict = {}
+    
+    for r in replacements:
+        if r[1] in replacement_dict.keys():
+            print("error", r[1])
+        replacement_dict[r[1]] = r[0]
+    
+    return replacement_dict
+
+def reduce_molecule(molecule: str, replacement_dict: dict[str, str]) -> str:
+    for key in replacement_dict.keys():
+        
+        idx = molecule.find(key)
+        if idx != -1:
+            temp_molecule = molecule.replace(key, replacement_dict[key], 1)
+            if replacement_dict[key] == "e" and temp_molecule != "e":
+                continue
+            else:
+                return temp_molecule
+
+    raise Exception("Couldn't reduce molecule")
 
 def run() -> None:
     replacements, final_molecule = parse_input(input)
 
     print(replacements)
     print(final_molecule)
+    
+    replacement_dict = parse_replacements(replacements)
+    molecule = final_molecule
+    
+    steps = 0
+    
+    while molecule != "e":
+        molecule = reduce_molecule(molecule, replacement_dict)
+        steps += 1
+    
 
-    result = 0
+    result = steps
     return str(result)
 
 
